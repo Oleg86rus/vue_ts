0546bcb81b5b7af75828c766483c7d5bad95f9f9
oleg086rus <o.pototskiy@yandex.ru>
2023-01-09 02:16:00 +0500

    fix start

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="navbar">
+    <h3>Vue + TypeScript</h3>
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Главная</router-link>
+      </li>
+      <li>
+        <router-link to="/users">Пользователи</router-link>
+      </li>
+    </ul>
+  </header>
+  <main class="container with-nav">
+    <router-view/>
+  </main>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
